19d8c5d63dd090edf7d701349f28ac844af05966
Nnamani John <nnamanijohn123@gmail.com>
2024-12-06 11:36:47 +0100

    car sides

--- src/car.ts
+++ src/car.ts
@@ -10,6 +10,7 @@ class Car {
     friction: number;
     angle: number;
     sensor: Sensor;
+    polygon: Point[];
 
     constructor(
         public x: number,
@@ -24,27 +25,50 @@ class Car {
         this.angle = 0;
         this.controls = new Controls();
         this.sensor = new Sensor(this);
+        this.polygon = this.createPolygon();
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
+        ctx.beginPath()
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
+        ctx.fill()
 
-        ctx.beginPath();
-        ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
-        ctx.fill();
-
-        ctx.restore();
         this.sensor.draw(ctx);
     }
 
     update(roadBorders: Point[][]) {
         this.move();
+        this.polygon = this.createPolygon();
         this.sensor.update(roadBorders);
     }
 
-    protected move() {
+    private createPolygon(): Point[] {
+        const points = []
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan(this.width / this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad,
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad,
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + Math.PI - alpha) * rad,
+            y: this.y - Math.cos(this.angle + Math.PI - alpha) * rad,
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + Math.PI + alpha) * rad,
+            y: this.y - Math.cos(this.angle + Math.PI + alpha) * rad,
+        });
+        return points;
+    }
+
+    private move() {
         if (this.controls.forward) {
             this.speed += this.acceleration;
         }
